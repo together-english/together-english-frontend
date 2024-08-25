14aeef5156d25fa96e1fa96c885468034dabe2b1
kimJongWan <kimdeiz@naver.com>
2024-08-25 12:26:57 +0900

    redux 초기 세팅

---
+++ store/copy/reducers.ts
@@ -0,0 +1,10 @@
+import * as T from "./types";
+
+const initializeStore: T.State = {};
+
+export const reducer = (
+  state: T.State = initializeStore,
+  action: T.Actions
+) => {
+  return state;
+};
