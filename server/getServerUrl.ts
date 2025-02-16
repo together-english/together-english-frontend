62a9dab47b001e2da564cca00d06836830aaee56
Bell_Bell <47379176+3210439@users.noreply.github.com>
2025-02-16 21:38:30 +0900

    server 요청 url 수정

--- server/getServerUrl.ts
+++ server/getServerUrl.ts
@@ -1,5 +1,5 @@
 export const getServerUrl = (path: string) => {
-  //const host = 'https://api.together-english.com'
-  const host = 'http://localhost'
+  const host = 'https://api.together-english.com'
+  //const host = 'http://localhost'
   return [host, path].join('')
 }
